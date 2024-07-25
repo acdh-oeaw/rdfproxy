d01fe29277c0036ee236e437be592808151643a3
Lukas Plank <lupl@tuta.io>
2024-07-25 12:36:10 +0200

    feat: Expose more symbols over package interface
    Expose get_bindings_from_query_result and
    instantiate_model_from_kwargs in rdfproxy.__init__.py; these functions
    can be useful e.g. for model_constructor callables and should be easily accessible.
    
    closes: #11

--- rdfproxy/__init__.py
+++ rdfproxy/__init__.py
@@ -1,1 +1,6 @@
 from rdfproxy.adapter import SPARQLModelAdapter
+from rdfproxy.utils._types import _TModelConstructorCallable, _TModelInstance
+from rdfproxy.utils.utils import (
+    get_bindings_from_query_result,
+    instantiate_model_from_kwargs,
+)
