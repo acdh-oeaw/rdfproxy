f4c04487ba86699d41483a1f8fc8b47a01d2de08
Lukas Plank <lupl@tuta.io>
2024-07-25 13:20:29 +0200

    fix: Type hint cleanup
    Replaced ModelInstance type with
    rdfproxy.utils._types._TMOdelInstance. ModelInstance was defined using
    PEP 695 before but the type will be used all over the code base and
    should have a central definition.
    
    Fixed _TModelConstructorCallable type to be a Callable expecting a
    SPARQLWrapper.QueryResult parameter and returning an Iterable of
    Pydantic model instances.
    
    closes: #10

--- rdfproxy/utils/_types.py
+++ rdfproxy/utils/_types.py
@@ -1,17 +1,17 @@
 """Type definitions for rdfproxy."""
 
+from collections.abc import Iterable
 from typing import Annotated, Protocol, TypeVar, runtime_checkable
 
+from SPARQLWrapper import QueryResult
 from pydantic import BaseModel
 
 
-_TModelInstance: Annotated[TypeVar, "Type defintion for Pydantic model instances."] = (
-    TypeVar("_TModelInstance", bound=BaseModel)
-)
+_TModelInstance = TypeVar("_TModelInstance", bound=BaseModel)
 
 
 @runtime_checkable
 class _TModelConstructorCallable[ModelType: BaseModel](Protocol):
     """Callback protocol for model constructor callables."""
 
-    def __call__(self, **kwargs) -> ModelType: ...
+    def __call__(self, query_result: QueryResult) -> Iterable[ModelType]: ...
