b73fc95dee4ce6caaf30ba6b2138334557b8afb9
Lukas Plank <lupl@tuta.io>
2024-07-25 13:20:29 +0200

    fix: Type hint cleanup
    Replaced ModelInstance type with
    rdfproxy.utils._types._TMOdelInstance. ModelInstance was defined using
    PEP 695 before but the type will be used all over the code base and
    should have a central definition.
    
    Fixed _TModelConstructorCallable type to be a Callable expecting a
    SPARQLWrapper.QueryResult parameter and returning an Iterable of
    Pydantic model instances.
    
    closes: #10

--- rdfproxy/adapter.py
+++ rdfproxy/adapter.py
@@ -1,17 +1,19 @@
 """SPARQLModelAdapter class for QueryResult to Pydantic model conversions."""
 
-from collections.abc import Sequence
+from collections.abc import Iterable
+from typing import cast
 
 from SPARQLWrapper import JSON, QueryResult, SPARQLWrapper
 from pydantic import BaseModel
-from rdfproxy.utils._types import _TModelConstructorCallable
+
+from rdfproxy.utils._types import _TModelConstructorCallable, _TModelInstance
 from rdfproxy.utils.utils import (
     get_bindings_from_query_result,
     instantiate_model_from_kwargs,
 )
 
 
-class SPARQLModelAdapter[ModelType: BaseModel]:
+class SPARQLModelAdapter:
     """Adapter/Mapper for QueryResult to Pydantic model conversions."""
 
     def __init__(self, sparql_wrapper: SPARQLWrapper) -> None:
@@ -20,19 +22,25 @@ class SPARQLModelAdapter[ModelType: BaseModel]:
         if self.sparql_wrapper.returnFormat != "json":
             self.sparql_wrapper.setReturnFormat(JSON)
 
-    def __call__(self, query: str, model_constructor) -> Sequence[ModelType]:
+    def __call__(
+        self,
+        query: str,
+        model_constructor: type[_TModelInstance] | _TModelConstructorCallable,
+    ) -> Iterable[_TModelInstance]:
         self.sparql_wrapper.setQuery(query)
         query_result: QueryResult = self.sparql_wrapper.query()
 
         if isinstance(model_constructor, type(BaseModel)):
+            model_constructor = cast(type[_TModelInstance], model_constructor)
+
             bindings = get_bindings_from_query_result(query_result)
-            models: list[ModelType] = [
+            models: list[_TModelInstance] = [
                 instantiate_model_from_kwargs(model_constructor, **binding)
                 for binding in bindings
             ]
 
         elif isinstance(model_constructor, _TModelConstructorCallable):
-            models: list[ModelType] = model_constructor(query_result)
+            models: Iterable[_TModelInstance] = model_constructor(query_result)
 
         else:
             raise TypeError(
