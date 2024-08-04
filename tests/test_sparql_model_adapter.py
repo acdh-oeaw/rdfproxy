06e057f41d4e775dbc48f856033a8d609dd56700
Lukas Plank <lupl@tuta.io>
2024-08-04 18:00:14 +0200

    test: Implement basic test for ungrouped query_paginate
    The test applies ungrouped pagination with
    SPARQLModelAdapter.query_paginate and checks if the return results
    comply by the pagination constraints.

--- tests/test_sparql_model_adapter.py
+++ tests/test_sparql_model_adapter.py
@@ -1,8 +1,7 @@
 """Pytest entry point for rdfproxy.SPARQLModelAdapter tests."""
 
-import pytest
-
 from hypothesis import given, settings
+import pytest
 from rdfproxy import SPARQLModelAdapter
 from rdfproxy.utils._exceptions import UndefinedBindingException
 from tests.data.models import ComplexModel
@@ -59,7 +58,7 @@ def test_sparql_model_adapter_grouping_basic():
 
 @pytest.mark.remote
 @given(var=public_variable_names)
-@settings(max_examples=10)
+@settings(max_examples=5)
 def test_sparql_model_adapter_grouping_basic_fail(var):
     query = """
     select ?x ?y ?a ?p ?z
@@ -81,3 +80,32 @@ def test_sparql_model_adapter_grouping_basic_fail(var):
     if var not in ["x", "y", "a", "p", "z"]:
         with pytest.raises(UndefinedBindingException):
             group = adapter.query_group_by(var)  # noqa: F841
+
+
+@pytest.mark.remote
+def test_sparql_model_adapter_ungrouped_pagination_basic():
+    query = """
+    select ?x ?y ?a ?p
+    where {
+      values (?x ?y ?a ?p) {
+        (1 2 "a value" "p value")
+        (1 3 "another value" "p value 2")
+        (2 4 "yet anoter value" "p value 3")
+        (2 5 "yet anoter value 2" "p value 4")
+      }
+    }
+    """
+
+    adapter = SPARQLModelAdapter(
+        endpoint="https://query.wikidata.org/bigdata/namespace/wdq/sparql",
+        query=query,
+        model=ComplexModel,
+    )
+
+    page_1 = adapter.query_paginate(page=1, size=2)
+    page_2 = adapter.query_paginate(page=2, size=3)
+    page_3 = adapter.query_paginate(page=3, size=1)
+
+    assert len(page_1) == 2
+    assert len(page_2) == 1
+    assert len(page_3) == 1
