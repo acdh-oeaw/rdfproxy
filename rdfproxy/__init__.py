47e4222431e7b0498ebc5962d6cb195b4c84075c
Lukas Plank <lupl@tuta.io>
2024-08-16 06:58:24 +0200

    feat: redesign SPARQLModelAdapter and implement query interface
    This commit holds a substantial redesign of the SPARQLModelAdapter
    class and also introduces additional functionality.
    
    All currently available functionality is now condensed in the sole
    public method of the class, SPARQLModelAdapter.query, which is
    controlled by its parameters and parameter combinations.

--- rdfproxy/__init__.py
+++ rdfproxy/__init__.py
@@ -1,5 +1,6 @@
 from rdfproxy.adapter import SPARQLModelAdapter
 from rdfproxy.utils._types import _TModelConstructorCallable, _TModelInstance
+from rdfproxy.utils.models import Page
 from rdfproxy.utils.utils import (
     get_bindings_from_query_result,
     instantiate_model_from_kwargs,
