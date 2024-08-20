2b471518f542ece73607a47be265d6f79191f294
Lukas Plank <lupl@tuta.io>
2024-08-20 13:29:53 +0200

    feat: redesign SPARQLModelAdapter class
    The redesign introduces major class API changes:
    
    1. Initialization now takes the endpoint, query and model class directly, this simplifies
    class usage and allows for better state retention in the instance.
    
    2. functionality previously defined in
    SPARQLModelAdapter.__call__ is transposed to SPARQLModelAdapter.query.
    
    Closes: #38.

--- rdfproxy/utils/_types.py
+++ rdfproxy/utils/_types.py
@@ -15,3 +15,24 @@ class _TModelConstructorCallable(Protocol[_TModelInstance]):
     """Callback protocol for model constructor callables."""
 
     def __call__(self, query_result: QueryResult) -> Iterable[_TModelInstance]: ...
+
+
+class SPARQLBinding(str):
+    """SPARQLBinding type for explicit SPARQL binding to model field allocation.
+
+    This type's intended use is with typing.Annotated in the context of a Pyantic field definition.
+
+    Example:
+
+        class Work(BaseModel):
+           name: Annotated[str, SPARQLBinding("title")]
+
+        class Person(BaseModel):
+            name: str
+            work: Work
+
+    This signals to the RDFProxy SPARQL-to-model mapping logic
+    to use the "title" SPARQL binding (not the "name" binding) to populate the Work.name field.
+    """
+
+    ...
--- rdfproxy/utils/utils.py
+++ rdfproxy/utils/utils.py
@@ -1,12 +1,12 @@
 """SPARQL/FastAPI utils."""
 
-from collections.abc import Iterator, Mapping
-from typing import cast
+from collections.abc import Iterator
+from typing import Any
 
 from SPARQLWrapper import QueryResult
 from pydantic import BaseModel
-from rdfproxy.utils._types import _TModelInstance
-from toolz import valmap
+from pydantic.fields import FieldInfo
+from rdfproxy.utils._types import SPARQLBinding, _TModelInstance
 
 
 def get_bindings_from_query_result(query_result: QueryResult) -> Iterator[dict]:
@@ -17,9 +17,9 @@ def get_bindings_from_query_result(query_result: QueryResult) -> Iterator[dict]:
             f"Received object with requestedFormat '{result_format}'."
         )
 
-    query_json = cast(Mapping, query_result.convert())
+    query_json: dict = query_result.convert()
     bindings = map(
-        lambda binding: valmap(lambda v: v["value"], binding),
+        lambda binding: {k: v["value"] for k, v in binding.items()},
         query_json["results"]["bindings"],
     )
 
@@ -30,6 +30,9 @@ def instantiate_model_from_kwargs(
     model: type[_TModelInstance], **kwargs
 ) -> _TModelInstance:
     """Instantiate a (potentially nested) model from (flat) kwargs.
+
+    More a more generic version of this function see upto.init_model_from_kwargs
+    https://github.com/lu-pl/upto?tab=readme-ov-file#init_model_from_kwargs.
 
     Example:
 
@@ -52,22 +55,36 @@ def instantiate_model_from_kwargs(
     print(model)  # p='p value' q=NestedModel(a='a value', b=SimpleModel(x=1, y=2))
     """
 
-    def _get_bindings(model: type[_TModelInstance], **kwargs) -> dict:
+    def _get_key_from_metadata(v: FieldInfo):
+        """Try to get a SPARQLBinding object from a field's metadata attribute.
+
+        Helper for _generate_binding_pairs.
+        """
+        try:
+            value = next(filter(lambda x: isinstance(x, SPARQLBinding), v.metadata))
+            return value
+        except StopIteration:
+            return None
+
+    def _generate_binding_pairs(
+        model: type[_TModelInstance], **kwargs
+    ) -> Iterator[tuple[str, Any]]:
         """Get the bindings needed for model instantation.
 
         The function traverses model.model_fields
-        and constructs a bindings dict by either getting values from kwargs or field defaults.
+        and constructs binding pairs by either getting values from kwargs or field defaults.
         For model fields the recursive clause runs.
-
-        Note: This needs exception handling and proper testing.
         """
-        return {
-            k: (
-                v.annotation(**_get_bindings(v.annotation, **kwargs))
-                if isinstance(v.annotation, type(BaseModel))
-                else kwargs.get(k, v.default)
-            )
-            for k, v in model.model_fields.items()
-        }
+        for k, v in model.model_fields.items():
+            if isinstance(v.annotation, type(BaseModel)):
+                value = v.annotation(
+                    **dict(_generate_binding_pairs(v.annotation, **kwargs))
+                )
+            else:
+                binding_key = _get_key_from_metadata(v) or k
+                value = kwargs.get(binding_key, v.default)
 
-    return model(**_get_bindings(model, **kwargs))
+            yield k, value
+
+    bindings = dict(_generate_binding_pairs(model, **kwargs))
+    return model(**bindings)
