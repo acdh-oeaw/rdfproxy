8d3ea58581018c4bd97813ddb2d56c67ef109815
Lukas Plank <lupl@tuta.io>
2024-08-21 15:46:14 +0200

    test: add tests for explicit SPARQL binding allocation
    Concerns: #56

--- tests/test_instantiate_model_from_kwargs.py
+++ tests/test_instantiate_model_from_kwargs.py
@@ -6,6 +6,7 @@ from rdfproxy import instantiate_model_from_kwargs
 from tests.data.init_model_from_kwargs_parameters import (
     init_model_from_kwargs_parameters,
 )
+from tests.data.models import Person
 
 
 @pytest.mark.parametrize(("model", "kwargs"), init_model_from_kwargs_parameters)
@@ -14,3 +15,12 @@ def test_init_model_from_kwargs(model, kwargs):
     for _kwargs in kwargs:
         model_instance = instantiate_model_from_kwargs(model, **_kwargs)
         assert isinstance(model_instance, model)
+
+
+def test_explicit_sparql_binding_allocation():
+    bindings: dict = {"title": "Test Title", "name": "Test Name"}
+    expected: dict = {"name": "Test Name", "work": {"name": "Test Title"}}
+
+    model = instantiate_model_from_kwargs(Person, **bindings)
+
+    assert expected == model.dict()
