4d445956fd4fee2c4d50c5be49719fac0d32f100
Lukas Plank <lupl@tuta.io>
2024-08-27 12:54:42 +0200

    refactor: declutter public library interface
    The public library interface currently only needs to expose
    SPARQLModelAdapter, SPARQLBinding and Page.

--- rdfproxy/__init__.py
+++ rdfproxy/__init__.py
@@ -1,6 +1,3 @@
 from rdfproxy.adapter import SPARQLModelAdapter  # noqa: F401
-from rdfproxy.utils._types import _TModelConstructorCallable, _TModelInstance  # noqa: F401
-from rdfproxy.utils.utils import (
-    get_bindings_from_query_result,  # noqa: F401
-    instantiate_model_from_kwargs,  # noqa: F401
-)
+from rdfproxy.utils._types import SPARQLBinding  # noqa: F401
+from rdfproxy.utils.models import Page  # noqa: F401
