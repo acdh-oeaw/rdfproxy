daf72cb9e409aa750954ecee55b7d8c65f5f41a8
Lukas Plank <lupl@tuta.io>
2024-10-24 16:33:16 +0200

    feat: implement model_bool hook for controlling model truthiness
    Model truthiness is an important metric for the rdfproxy grouping mechanism.
    Currently, a model is considered truthy if at least one of its fields is truthy.
    
    This is a sane default, yet certain frontend demands require different model truth conditions.
    
    The feature introduces a model_bool option in the model_config
    that allows client code to specify conditions/predicates for determining model truthiness.
    
    Closes #110, closes #112.

--- rdfproxy/utils/utils.py
+++ rdfproxy/utils/utils.py
@@ -1,7 +1,7 @@
 """SPARQL/FastAPI utils."""
 
 from collections.abc import Callable, Iterable
-from typing import Any, get_args, get_origin
+from typing import Any, TypeGuard, get_args, get_origin
 
 from pydantic import BaseModel
 from pydantic.fields import FieldInfo
@@ -9,7 +9,7 @@ from rdfproxy.utils._exceptions import (
     MissingModelConfigException,
     UnboundGroupingKeyException,
 )
-from rdfproxy.utils._types import SPARQLBinding
+from rdfproxy.utils._types import ModelBoolPredicate, SPARQLBinding, _TModelBoolValue
 
 
 def _is_type(obj: type | None, _type: type) -> bool:
@@ -70,3 +70,47 @@ def _get_group_by(model: type[BaseModel], kwargs: dict) -> str:
                 f"Applicable grouping keys: {', '.join(kwargs.keys())}."
             )
         return group_by
+
+
+def default_model_bool_predicate(model: BaseModel) -> bool:
+    """Default predicate for determining model truthiness.
+
+    Adheres to rdfproxy.utils._types.ModelBoolPredicate.
+    """
+    return any(dict(model).values())
+
+
+def _is_iterable_of_str(iterable: Iterable) -> TypeGuard[Iterable[str]]:
+    return (not isinstance(iterable, str)) and all(
+        map(lambda i: isinstance(i, str), iterable)
+    )
+
+
+def _get_model_bool_predicate_from_config_value(
+    model_bool_value: _TModelBoolValue,
+) -> ModelBoolPredicate:
+    """Get a model_bool predicate function given the value of the model_bool config setting."""
+    match model_bool_value:
+        case ModelBoolPredicate():
+            return model_bool_value
+        case str():
+            return lambda model: bool(dict(model)[model_bool_value])
+        case model_bool_value if _is_iterable_of_str(model_bool_value):
+            return lambda model: all(map(lambda k: dict(model)[k], model_bool_value))
+        case _:
+            raise TypeError(
+                "Argument for 'model_bool' must be of type ModelBoolPredicate | str | Iterable[str].\n"
+                f"Received {type(model_bool_value)}"
+            )
+
+
+def get_model_bool_predicate(model: BaseModel) -> ModelBoolPredicate:
+    """Get the applicable model_bool predicate function given a model."""
+    if (model_bool_value := model.model_config.get("model_bool", None)) is None:
+        model_bool_predicate = default_model_bool_predicate
+    else:
+        model_bool_predicate = _get_model_bool_predicate_from_config_value(
+            model_bool_value
+        )
+
+    return model_bool_predicate
