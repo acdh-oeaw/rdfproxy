ce39fa558d4bef86c8dd73b744d78592f2bb9613
Lukas Plank <lupl@tuta.io>
2024-10-30 13:15:47 +0100

    refactor(exceptions): remove unused exceptions
    Closes #124.

--- rdfproxy/utils/_exceptions.py
+++ rdfproxy/utils/_exceptions.py
@@ -1,14 +1,6 @@
 """Custom exceptions for RDFProxy."""
 
 
-class UndefinedBindingException(KeyError):
-    """Exception for indicating that a requested key could not be retrieved from a SPARQL binding mapping."""
-
-
-class InterdependentParametersException(Exception):
-    """Exceptiono for indicating that two or more parameters are interdependent."""
-
-
 class MissingModelConfigException(Exception):
     """Exception for indicating that an expected Config class is missing in a Pydantic model definition."""
 
