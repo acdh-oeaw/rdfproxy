72a273d028ee3271f3d39e6d3e43d65b95ec8516
Lukas Plank <lupl@tuta.io>
2024-11-28 06:01:22 +0100

    feat: use query parameter model for SPARQLModelAdapter.query
    Using a query parameter model for SPARQLModelAdapter.query has many
    advantages, e.g. enhanced OpenAPI/Swagger definitions for FastAPI code
    and runtime validation for query parameters.
    
    Also, the query parameter model defines a single extension interface
    for query parameters.
    
    Closes #150.

--- rdfproxy/utils/models.py
+++ rdfproxy/utils/models.py
@@ -2,7 +2,7 @@
 
 from typing import Generic
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from rdfproxy.utils._types import _TModelInstance
 
 
@@ -21,3 +21,13 @@ class Page(BaseModel, Generic[_TModelInstance]):
     size: int
     total: int
     pages: int
+
+
+class QueryParameters(BaseModel):
+    """Query parameter model for SPARQLModelAdapter.query.
+
+    See https://fastapi.tiangolo.com/tutorial/query-param-models/
+    """
+
+    page: int = Field(default=1, gt=0)
+    size: int = Field(default=100, ge=1)
