42661293a89ba8bb454c4b8dd125122107a2ec8a
Lukas Plank <lupl@tuta.io>
2024-11-30 19:46:26 +0100

    chore(init): ignore E402 in rdfproxy.__init__
    Package-wide beartype runtime checking requires
    'beartype_this_package' to run *before* all other __init__ imports.
    
    This is not PEP-8 compliant, see https://peps.python.org/pep-0008/#imports.

--- rdfproxy/__init__.py
+++ rdfproxy/__init__.py
@@ -1,9 +1,8 @@
 from beartype.claw import beartype_this_package
-beartype_this_package()     
 
-from rdfproxy.adapter import SPARQLModelAdapter  # noqa: F401
-from rdfproxy.mapper import ModelBindingsMapper  # noqa: F401
-from rdfproxy.utils._types import SPARQLBinding  # noqa: F401
-from rdfproxy.utils.models import Page  # noqa: F401
+beartype_this_package()
 
-
+from rdfproxy.adapter import SPARQLModelAdapter  # noqa: F401, E402
+from rdfproxy.mapper import ModelBindingsMapper  # noqa: F401, E402
+from rdfproxy.utils._types import SPARQLBinding  # noqa: F401, E402
+from rdfproxy.utils.models import Page  # noqa: F401, E402
