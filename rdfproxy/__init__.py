a65c42ad9fbe0a862c022543a588175c7a399594
Lukas Plank <lupl@tuta.io>
2024-11-30 17:45:25 +0100

    feat: add beartype checking for rdfproxy
    beartype offers high performance runtime type checking based on type annotations.
    Runtime checking can contribute to a healthier code base (see fixes)
    and also allows fast failures with meaningful exceptions for library users.
    
    Closes #152.

--- rdfproxy/__init__.py
+++ rdfproxy/__init__.py
@@ -1,4 +1,9 @@
+from beartype.claw import beartype_this_package
+beartype_this_package()     
+
 from rdfproxy.adapter import SPARQLModelAdapter  # noqa: F401
 from rdfproxy.mapper import ModelBindingsMapper  # noqa: F401
 from rdfproxy.utils._types import SPARQLBinding  # noqa: F401
 from rdfproxy.utils.models import Page  # noqa: F401
+
+
