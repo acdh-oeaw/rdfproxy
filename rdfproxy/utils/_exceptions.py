3d71409ad121578925e404bc624a2aba7d3284ba
Lukas Plank <lupl@tuta.io>
2024-12-02 14:08:42 +0100

    feat: implement grouping by model field
    Currently, the group_by model_config parameter expects the name of a
    SPARQLBinding; the rationale for this behavior was, that grouping
    should theoretically also be possible for bindings that are not part
    of the model.
    
    Grouping is however primarily concerded with the model i.e. shapes
    definition, grouping by a model field is therefore more intuitive and
    conceptually more consistent.
    
    Grouping by bindings should not be part of the model is still possible by using
    pydantic.Field(exclude=True). This also forces backend implementers to
    be explicit about 'external' grouping keys.
    
    Closes #146.

--- rdfproxy/utils/_exceptions.py
+++ rdfproxy/utils/_exceptions.py
@@ -5,8 +5,8 @@ class MissingModelConfigException(Exception):
     """Exception for indicating that an expected Config class is missing in a Pydantic model definition."""
 
 
-class UnboundGroupingKeyException(Exception):
-    """Exception for indicating that no SPARQL binding corresponds to the requested grouping key."""
+class InvalidGroupingKeyException(Exception):
+    """Exception for indicating that an invalid grouping key has been encountered."""
 
 
 class QueryConstructionException(Exception):
