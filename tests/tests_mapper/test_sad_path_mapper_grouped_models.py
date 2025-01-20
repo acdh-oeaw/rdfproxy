663134efba1cd773fb5e648130475a478fd40d96
Lukas Plank <lupl@tuta.io>
2025-01-20 10:29:56 +0100

    test: mark model sanity tests xfail
    Some model sanity checking was implemented in the old
    ModelBindingsMapper, all checking should be done in a dedicated model
    sanity checking pipeline, see issue #108.
    
    So these tests - for now - are xfails.

--- tests/tests_mapper/test_sad_path_mapper_grouped_models.py
+++ tests/tests_mapper/test_sad_path_mapper_grouped_models.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel
 import pytest
+
+from pydantic import BaseModel
 from rdfproxy import ConfigDict, ModelBindingsMapper
 from rdfproxy.utils._exceptions import (
     InvalidGroupingKeyException,
@@ -17,11 +18,13 @@ class ModelMissingGroupByValue(BaseModel):
     x: list[int]
 
 
+@pytest.mark.xfail(reason="Not yet implemented, checks will run in model checkers.")
 def test_sad_path_adapter_missing_grouping_config():
     with pytest.raises(MissingModelConfigException):
         ModelBindingsMapper(ModelMissingGroupByConfig, {"x": 1}).get_models()
 
 
+@pytest.mark.xfail(reason="Not yet implemented, checks will run in model checkers.")
 def test_sad_path_adapter_missing_grouping_value():
     with pytest.raises(InvalidGroupingKeyException):
         ModelBindingsMapper(ModelMissingGroupByValue, {"x": 1}).get_models()
