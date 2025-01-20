0957ba0269ffed3dc34f1cd762847455f7d41d60
Lukas Plank <lupl@tuta.io>
2025-01-20 10:40:16 +0100

    fix(test): fix sketchy test for ModelBindingsMapper
    The expected data was actually wrong and passed a buggy behavior in
    the old ModelBindingsMapper. The change fixes the test to expect the
    correct result.
    
    Note that this test case is somewhat contrived,
    because the binding data is unordered and actual data from an
    RDFProxy-modified query for grouped models would be ordered. The
    refactored mapper uses a pd.DataFrame for grouping, so (unlike a
    possible low-level solution with itertools.groupby) the above case can
    be handled, because dataframes implement efficient grouping also across
    unordered row series.
    
    This is actually a quite powerful feature and makes the
    ModelBingingsMapper class generally useful, not just in the context of RDFProxy.

--- tests/tests_mapper/params/model_bindings_mapper_parameters.py
+++ tests/tests_mapper/params/model_bindings_mapper_parameters.py
@@ -219,10 +219,6 @@ nested_grouping_parameters = [
             },
             {"p": "p value 2", "q": [{"a": "a value 2", "b": [{"x": 1, "y": 2}]}]},
             {"p": "p value 3", "q": [{"a": "a value 3", "b": [{"x": 1, "y": 3}]}]},
-            {
-                "p": "p value 4",
-                "q": [{"a": "a value 1", "b": [{"x": 1, "y": 2}, {"x": 2, "y": 2}]}],
-            },
         ],
     )
 ]
