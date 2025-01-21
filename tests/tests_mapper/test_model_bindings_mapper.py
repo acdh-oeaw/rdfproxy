bcbb4c0a624e420cbf947371a79cb8a7412b0196
Lukas Plank <lupl@tuta.io>
2025-01-21 10:36:00 +0100

    test: implement empty model and default-only model mapper tests
    Concerns #181.

--- tests/tests_mapper/test_model_bindings_mapper.py
+++ tests/tests_mapper/test_model_bindings_mapper.py
@@ -8,6 +8,7 @@ from tests.tests_mapper.params.model_bindings_mapper_parameters import (
     author_array_collection_parameters,
     author_work_title_parameters,
     basic_parameters,
+    empty_default_only_model_parameters,
     grouping_nested_model_parameters,
     grouping_parameters,
     nested_grouping_parameters,
@@ -23,6 +24,7 @@ from tests.tests_mapper.params.model_bindings_mapper_parameters import (
         *author_work_title_parameters,
         *author_array_collection_parameters,
         *grouping_nested_model_parameters,
+        *empty_default_only_model_parameters,
     ],
 )
 def test_basic_model_bindings_mapper(model, bindings, expected):
