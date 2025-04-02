9be52fdefa922ebb91c3b5a3403c16fc4c2bacc2
Lukas Plank <lupl@tuta.io>
2025-04-02 14:50:37 +0200

    docs: refine model_traverse function doc string
    See https://github.com/acdh-oeaw/rdfproxy/issues/245#issuecomment-2772027467.

--- rdfproxy/utils/model_utils.py
+++ rdfproxy/utils/model_utils.py
@@ -19,10 +19,17 @@ def model_traverse(
     f: Callable[[type[BaseModel]], T],
     include_root_model: bool = True,
 ) -> Iterator[T]:
-    """Recursively traverse a model and apply a callable to all (sub)models.
+    """Recursively traverse a model and apply a callable to all applicable (sub)models.
 
-    If the include_root_model flag is set to True, the callable will be applied to the root model model as well.
+    Submodels are applicable if they are significant according to RDFProxy semantics.
+
+    If the include_root_model flag is set to True,
+    the callable will be applied to the root model as well.
     Recursive calls intentionally do not pass on the include_root_model flag.
+
+    Note that currently all models of a model union are traversed, although only
+    the first model of a model union will be considered by the RDFProxy mapper.
+    This behavior is evaluated and considered in #245.
     """
     if include_root_model:
         yield f(model)
