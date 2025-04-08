a188c6d3de7b0b79fda3e546504f7c6a3f209178
Lukas Plank <lupl@tuta.io>
2025-04-08 16:36:17 +0200

    feat: support rdflib.Graph as a SPARQL target
    The change introduces functionality for targeting an rdflib.Graph
    instance with rdfproxy.SPARQLModelAdapter and rdfproxy.SPARQLWrapper.
    
    Closes #221.

--- rdfproxy/sparqlwrapper.py
+++ rdfproxy/sparqlwrapper.py
@@ -1,22 +1,48 @@
 from collections.abc import Iterator
-from typing import Any
+import json
+from typing import Any, cast
+
+from rdflib import BNode, Graph, Literal, URIRef, XSD
 
 import httpx
-from rdflib import BNode, Literal, URIRef, XSD
 
 
 class SPARQLWrapper:
     """Simple httpx-based SPARQLWrapper implementaton for RDFProxy."""
 
-    def __init__(self, endpoint: str):
-        self.endpoint = endpoint
+    def __init__(self, target: str | Graph):
+        self.target = target
 
     def query(self, query: str) -> Iterator[dict[str, Any]]:
-        """Run a SPARQL query against endpoint and return an Iterator of flat result mappings."""
+        """Run a SPARQL query against target and return an Iterator of flat result mappings."""
+        if isinstance(self.target, Graph):
+            return self._query_graph_object(query)
+        elif isinstance(self.target, str):
+            return self._query_remote_endpoint(query)
+        else:  # pragma: no cover
+            raise Exception("Parameter 'target' expects argument of type str | Graph.")
+
+    def _query_remote_endpoint(self, query: str) -> Iterator[dict[str, Any]]:
+        """Run a SPARQL query against a remote endpoint."""
         result: httpx.Response = self._httpx_run_sparql_query(query)
         result.raise_for_status()
 
         return self._get_bindings_from_json_response(result.json())
+
+    def _query_graph_object(self, query: str) -> Iterator[dict[str, Any]]:
+        """Run a SPARQL query against an rdflib.Graph instance.
+
+        Note: rdflib.query.Result.serialize has a union return type;
+        the return type can be safely cast though, because the union-relevant
+        code path depends on the destination parameter, which isn't used here.
+        """
+        assert isinstance(self.target, Graph)  # type narrow
+
+        _result = self.target.query(query)
+        _serialized = cast(bytes, _result.serialize(format="json"))
+        _result_json = json.loads(_serialized)
+
+        return self._get_bindings_from_json_response(_result_json)
 
     @staticmethod
     def _get_bindings_from_json_response(
@@ -64,13 +90,15 @@ class SPARQLWrapper:
 
     def _httpx_run_sparql_query(self, query: str) -> httpx.Response:
         """Run a query against endpoint using httpx.post."""
+        assert isinstance(self.target, str)
+
         data = {"output": "json", "query": query}
         headers = {
             "Accept": "application/sparql-results+json",
         }
 
         response = httpx.post(
-            self.endpoint,
+            self.target,
             headers=headers,
             data=data,
         )
