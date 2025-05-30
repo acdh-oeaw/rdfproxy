fc8adac40fca3671f8c4431679eb32469a7ae14e
Lukas Plank <lupl@tuta.io>
2025-05-30 09:23:51 +0200

    feat: update and publish RDFProxy exceptions
    The change bases all RDFProxy custom exceptions on a common
    RDFProxyBaseException class and renames exceptions that previously had
    "RDFProxy" in their names.
    
    Also, also RDFProxy exceptions are made public symbols in the package
    top-level namespace so that client code can easily access them.
    
    Closes #282.

--- rdfproxy/__init__.py
+++ rdfproxy/__init__.py
@@ -3,3 +3,4 @@ from rdfproxy.mapper import ModelBindingsMapper  # noqa: F401
 from rdfproxy.sparqlwrapper import SPARQLWrapper  # noqa: F401
 from rdfproxy.utils._types import ConfigDict, SPARQLBinding  # noqa: F401
 from rdfproxy.utils.models import Page, QueryParameters  # noqa: F401
+from rdfproxy.utils._exceptions import *  # noqa: F403
