8749aeeeeb90e8f99c011782d8038a3130db0f71
Lukas Plank <lupl@tuta.io>
2025-06-25 19:21:30 +0200

    feat: replace OrderableFieldsBindingsMap with ModelSPARQLMap
    The change replaces the incomplete OrderableFieldsBindingsMap class
    with a more capable mapping: ModelSPARQLMap.
    
    ModelSPARQLMap (optionally recursively) traverses the field
    information of a given Pydantic model and aims to determine
    SPARQL-bound fields by applying certain heuristics, e.g. by checking
    against the _TSPARQLBoundField type.
    
    Note that in order to unambiguously determine SPARQL-bound fields of an
    RDFProxy-compliant model, one would need to check against the SPARQL
    query projection. Since the QueryParameters model needs to know SPARQL-bound
    fields at class parametrization time (i.e. actually metaclass
    runtime), knowledge of the query projection is far out of reach at
    this early point in computation time.

--- rdfproxy/utils/utils.py
+++ rdfproxy/utils/utils.py
@@ -2,15 +2,18 @@
 
 from collections import UserDict
 from collections import deque
-from collections.abc import Callable, Hashable
+from collections.abc import Callable, Hashable, Iterator
 from functools import partial
 from itertools import islice
-from typing import Any, Generic, NoReturn, Self, TypeVar
+from typing import Annotated, Any, Generic, NoReturn, Self, TypeVar, get_args
 
+from pydantic import BaseModel
+from pydantic.fields import FieldInfo
 from rdfproxy.utils._types import SPARQLBinding, _TModelInstance
 from rdfproxy.utils.type_utils import (
-    _is_list_static_type,
     _is_pydantic_model_static_type,
+    _is_pydantic_model_union_static_type,
+    _is_sparql_bound_field_type,
 )
 
 
@@ -49,34 +52,103 @@ class FieldsBindingsMap(UserDict):
         }
 
 
-class OrderableFieldsBindingsMap(FieldsBindingsMap):
-    """Recursive FieldsBindingsMap that generates namespaced key entries for orderable fields."""
+class ModelSPARQLMap(UserDict):
+    """Mapping for resolving SPARQL-bound model fields against SPARQLBinding aliases.
+
+    A SPARQL-bound type is either a subtype of _TSPARQLBoundField
+    or a union type of subtypes of _TSPARQLBoundField or None.
+
+    If recursve=True, model and model union typed fields are resolved recursively.
+    For nested models, ModelSPARQLMap keys get namespaced.
+
+    ModelSPARQLMap.reverse allows reverse lookup (i.e. from SPARQLBindings to model fields).
+    """
+
+    def __init__(self, model: type[_TModelInstance], recursive: bool = False) -> None:
+        self.model = model
+        self.recursive = recursive
+
+        self.data = self._get_model_sparql_mapping()
+
+    @property
+    def reverse(self) -> dict[str, str]:
+        """Reverse lookup map from SPARQL bindings to model fields."""
+        reverse = {v: k for k, v in self.data.items()}
+        return reverse
 
     def __missing__(self, key: Hashable) -> NoReturn:
-        orderable_fields = [f"'{k}'" for k in self.data.keys()]
+        sparql_bound_fields = [f"'{k}'" for k in self.data.keys()]
         model_name = self.model.__name__
 
         raise ValueError(
-            f"Parameter '{key}' does not reference an orderable field of model '{model_name}'.\n"
-            f"Applicable values for order_by: {', '.join(orderable_fields)}."
+            f"Parameter '{key}' does not reference a SPARQL-bound field of model '{model_name}'.\n"
+            f"Applicable values: {', '.join(sparql_bound_fields) if sparql_bound_fields else 'none'}."
         )
 
+    # this is also used in FieldsBindingsMap
     @staticmethod
-    def _get_field_binding_mapping(model: type[_TModelInstance]) -> dict[str, str]:
-        """Resolve model fields against rdfproxy.SPARQLBindings."""
+    def _get_sparql_bound_field_value(field_name: str, field_info: FieldInfo) -> str:
+        """Resolve the SPARQL binding name given model field information.
 
-        def _construct_bindings(model, top_level=True):
-            bindings_map = FieldsBindingsMap(model)
+        Empty SPARQLBinding objects are supported for additional/future semantics and fall back to the field_name.
+        """
+        _field_metadata = filter(
+            lambda x: isinstance(x, SPARQLBinding) and x,
+            field_info.metadata,
+        )
 
-            for k, v in model.model_fields.items():
-                # note: check for model union type might be required in the future
-                if _is_pydantic_model_static_type(v.annotation):
-                    yield from _construct_bindings(v.annotation, top_level=False)
+        return next(_field_metadata, field_name)
 
-                elif not _is_list_static_type(v.annotation):
-                    yield (k if top_level else f"{model.__name__}.{k}", bindings_map[k])
+    def _construct_mapping(
+        self, model: type[BaseModel], top_level: bool = True
+    ) -> Iterator[tuple[str, str]]:
+        """Recursive generator for Model-SPARQL mapping construction.
 
-        return dict(_construct_bindings(model))
+        The iterator traverses model fields to determine and yield SPARQL-bound fields;
+        if recursve=True, model and model union typed fields are resolved recursively.
+        """
+
+        fields_map: Annotated[
+            Iterator[tuple[str, FieldInfo]],
+            """Pydantic model_fields items data with default value fields filtered out.
+
+            Model union types are excluded from default value filtering to allow for recursion.
+            """,
+        ] = (
+            (field_name, field_info)
+            for field_name, field_info in model.model_fields.items()
+            if field_info.is_required()
+            or _is_pydantic_model_union_static_type(field_info.annotation)
+        )
+
+        for field_name, field_info in fields_map:
+            if self.recursive and _is_pydantic_model_union_static_type(
+                model_union := field_info.annotation
+            ):
+                union_model: type[BaseModel] = next(
+                    filter(
+                        _is_pydantic_model_static_type,
+                        get_args(model_union),
+                    )
+                )
+                yield from self._construct_mapping(model=union_model, top_level=False)
+
+            elif self.recursive and _is_pydantic_model_static_type(
+                nested_model := field_info.annotation
+            ):
+                yield from self._construct_mapping(model=nested_model, top_level=False)
+
+            elif _is_sparql_bound_field_type(field_info.annotation):
+                yield (
+                    field_name if top_level else f"{model.__name__}.{field_name}",
+                    self._get_sparql_bound_field_value(field_name, field_info),
+                )
+            else:
+                continue
+
+    def _get_model_sparql_mapping(self) -> dict[str, str]:
+        """Construct a Model-SPARQL mapping given a model."""
+        return dict(self._construct_mapping(model=self.model))
 
 
 def compose_left(*fns: Callable[[T], T]) -> Callable[[T], T]:
