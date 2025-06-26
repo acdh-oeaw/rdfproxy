2a20a6aedb39ec98891570223bd37db3b3f54e38
Lukas Plank <lupl@tuta.io>
2025-06-26 12:21:16 +0200

    test: implement tests for _is_sparql_bound_field_type

--- tests/unit/tests_type_utils/test_type_utils.py
+++ tests/unit/tests_type_utils/test_type_utils.py
@@ -1,14 +1,21 @@
 """Unit tests for the type_utils module."""
 
-from typing import Optional, Union
+import datetime
+import decimal
+from typing import Annotated, Optional, Union
+from xml.dom.minidom import Document
 
+from pydantic import AnyHttpUrl, AnyUrl
 from pydantic import BaseModel, create_model
 import pytest
+from rdflib import BNode, Literal, URIRef
+from rdflib.xsd_datetime import Duration
 from rdfproxy.utils.type_utils import (
     _is_list_pydantic_model_static_type,
     _is_list_static_type,
     _is_pydantic_model_static_type,
     _is_pydantic_model_union_static_type,
+    _is_sparql_bound_field_type,
 )
 
 
@@ -73,6 +80,55 @@ pydantic_model_union_static_type_true_parameters = [
     Union[str, None, Model],
 ]
 
+sparql_bound_field_type_parameters = [
+    # _TSPARQLBindingValue
+    URIRef,
+    BNode,
+    Literal,
+    None,
+    datetime.date,
+    datetime.datetime,
+    datetime.time,
+    datetime.timedelta,
+    Duration,
+    bytes,
+    bool,
+    int,
+    float,
+    decimal.Decimal,
+    Document,
+    # _TSPARQLBoundField
+    str,
+    AnyUrl,
+    # some subtypes
+    AnyHttpUrl,
+    # some unions
+    str | None,
+    URIRef | BNode,
+    URIRef | BNode | Literal,
+    # annotated types
+    Annotated[int, ""],
+    Annotated[int | str | None, ""],
+]
+
+sparql_bound_field_type_false_parameters = [
+    list[int],
+    list[Model],
+    tuple[str],
+    Model,
+    Annotated[Model, ""],
+    Annotated[list[Model], ""],
+]
+
+
+sparql_bound_field_type_instance_fail_parameters = [
+    1,
+    "1",
+    URIRef("https://test.org"),
+    object(),
+    datetime.datetime.now(),
+]
+
 
 @pytest.mark.parametrize("obj", pydantic_model_static_type_true_parameters)
 def test_is_pydantic_model_static_type_true(obj):
@@ -104,3 +160,19 @@ def test_is_pydantic_model_union_static_type_true(obj):
 @pytest.mark.parametrize("obj", list_static_type_false_parameters)
 def test_is_pydantic_model_union_static_type_false(obj):
     assert not _is_pydantic_model_union_static_type(obj)
+
+
+@pytest.mark.parametrize("obj", sparql_bound_field_type_parameters)
+def test_is_sparql_bound_field_type(obj):
+    assert _is_sparql_bound_field_type(obj)
+
+
+@pytest.mark.parametrize("obj", sparql_bound_field_type_false_parameters)
+def test_is_sparql_bound_field_type_false(obj):
+    assert not _is_sparql_bound_field_type(obj)
+
+
+@pytest.mark.parametrize("obj", sparql_bound_field_type_instance_fail_parameters)
+def test_is_sparql_bound_field_type_instance_fail(obj):
+    with pytest.raises(ValueError):
+        _is_sparql_bound_field_type(obj)
