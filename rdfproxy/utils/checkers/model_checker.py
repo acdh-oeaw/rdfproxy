5d889b37cd9d68adae30da667e96d95abf44ada0
Lukas Plank <lupl@tuta.io>
2025-06-30 19:37:58 +0200

    refactor: implement Visitor pattern for model checks
    The change replaces the modle_traverse function with a Visitor
    implementation; this allows for more efficient model traversal and
    application of checks either for root models only, submodels only or
    all models in a nested Pydantic model.

--- rdfproxy/utils/checkers/model_checker.py
+++ rdfproxy/utils/checkers/model_checker.py
@@ -1,54 +1,24 @@
 """Functionality for performing RDFProxy-compliance checks on Pydantic models."""
 
-from collections.abc import Callable
-
-from rdfproxy.utils._types import _TModelInstance
+from pydantic import BaseModel
 from rdfproxy.utils.checkers._model_checks import (
     _check_group_by_config,
     _check_model_bool_config_root_model,
     _check_model_bool_config_sub_models,
     _check_model_union_types,
 )
-from rdfproxy.utils.model_utils import model_traverse
-from rdfproxy.utils.utils import compose_left, consume
+from rdfproxy.utils.model_utils import ModelVisitor
+from rdfproxy.utils.utils import compose_left
 
 
-_TModelCheck = Callable[[type[_TModelInstance]], type[_TModelInstance]]
-
-
-def _root_model_check_runner(
-    model: type[_TModelInstance], *checks: _TModelCheck
-) -> None:
-    """Run checks only against the root model."""
-
-    compose_left(*checks)(model)
-
-
-def _sub_model_check_runner(
-    model: type[_TModelInstance], *checks: _TModelCheck
-) -> None:
-    """Run checks recursively; exclude the root model."""
-
-    composite = compose_left(*checks)
-    consume(
-        model_traverse(model, composite, include_root_model=False)
-    )  # exhaust for traversal
-
-
-def _full_model_check_runner(
-    model: type[_TModelInstance], *checks: _TModelCheck
-) -> None:
-    """Run checks recursively; include the root model."""
-
-    composite = compose_left(*checks)
-    consume(model_traverse(model, composite))  # exhaust for traversal
-
-
-def check_model(model: type[_TModelInstance]) -> type[_TModelInstance]:
+def check_model(model: type[BaseModel]) -> type[BaseModel]:
     """Main model checker: Run model checks using the appropriate check runners."""
 
-    _root_model_check_runner(model, _check_model_bool_config_root_model)
-    _sub_model_check_runner(model, _check_model_bool_config_sub_models)
-    _full_model_check_runner(model, _check_group_by_config, _check_model_union_types)
+    visitor = ModelVisitor(
+        model=model,
+        top_model_hook=_check_model_bool_config_root_model,
+        sub_model_hook=_check_model_bool_config_sub_models,
+        all_model_hook=compose_left(_check_group_by_config, _check_model_union_types),
+    )
 
-    return model
+    return visitor.visit()
--- rdfproxy/utils/model_utils.py
+++ rdfproxy/utils/model_utils.py
@@ -1,50 +1,82 @@
 """RDFProxy model utils."""
 
-from collections.abc import Callable, Iterator
-from typing import TypeVar, get_args
+from collections.abc import Callable
+from typing import Any, TypeAlias, get_args
 
 from pydantic import BaseModel
+from rdfproxy.utils._types import _TModelInstance
 from rdfproxy.utils.type_utils import (
     _is_list_pydantic_model_static_type,
     _is_pydantic_model_static_type,
     _is_pydantic_model_union_static_type,
 )
+from rdfproxy.utils.utils import identity
 
 
-T = TypeVar("T")
+_TModelHook: TypeAlias = Callable[[type[_TModelInstance]], Any]
 
 
-def model_traverse(
-    model: type[BaseModel],
-    f: Callable[[type[BaseModel]], T],
-    include_root_model: bool = True,
-) -> Iterator[T]:
-    """Recursively traverse a model and apply a callable to all applicable (sub)models.
+class ModelVisitor:
+    """Functional Visitor implementation for applying model hooks to a Pydantic model and its submodels.
 
-    Submodels are applicable if they are significant according to RDFProxy semantics.
+    Within RDFProxy, ModelVisitor is currently used for running model checks;
+    the class can be generally useful for applying side-effect callables to nested models though.
 
-    If the include_root_model flag is set to True,
-    the callable will be applied to the root model as well.
-    Recursive calls intentionally do not pass on the include_root_model flag.
-
-    Note that currently all models of a model union are traversed, although only
-    the first model of a model union will be considered by the RDFProxy mapper.
-    This behavior is evaluated and considered in #245.
+    Args:
+        model: The root Pydantic model class to visit.
+        top_model_hook: Callable hook to apply only to the top-level model.
+        sub_models_hook: Callable hook to apply only to nested models.
+        all_models_hook: Callable hook to apply to all models (top-level and nested).
     """
-    if include_root_model:
-        yield f(model)
 
-    for _, field_info in model.model_fields.items():
-        if _is_list_pydantic_model_static_type(list_model := field_info.annotation):
-            nested_model, *_ = get_args(list_model)
-            yield from model_traverse(nested_model, f)
+    def __init__(
+        self,
+        model: type[_TModelInstance],
+        top_model_hook: _TModelHook = identity,
+        sub_model_hook: _TModelHook = identity,
+        all_model_hook: _TModelHook = identity,
+    ) -> None:
+        self._model = model
 
-        elif _is_pydantic_model_static_type(nested_model := field_info.annotation):
-            yield from model_traverse(nested_model, f)
+        self._top_model_checks = (top_model_hook, all_model_hook)
+        self._sub_model_checks = (all_model_hook, sub_model_hook)
 
-        elif _is_pydantic_model_union_static_type(union := field_info.annotation):
-            for nested_model in filter(_is_pydantic_model_static_type, get_args(union)):
-                yield from model_traverse(nested_model, f)
+    def visit(self) -> type[BaseModel]:
+        """Run the registered validation checks against the model."""
+        return self._visit(self._model)
 
-        else:
-            continue
+    @staticmethod
+    def _run_checks(model: type[BaseModel], *checks: _TModelHook) -> None:
+        for check in checks:
+            check(model)
+
+    def _visit(self, model: type[BaseModel], top_level: bool = True) -> type[BaseModel]:
+        """Helper method for recursive model traversal.
+
+        The method visits a given model and applies registered validation checks.
+        """
+        if top_level:
+            self._run_checks(model, *self._top_model_checks)
+
+        for _, field_info in model.model_fields.items():
+            if _is_list_pydantic_model_static_type(list_model := field_info.annotation):
+                nested_model, *_ = get_args(list_model)
+                self._run_checks(nested_model, *self._sub_model_checks)
+                self._visit(nested_model, top_level=False)
+
+            elif _is_pydantic_model_static_type(nested_model := field_info.annotation):
+                self._run_checks(nested_model, *self._sub_model_checks)
+                self._visit(nested_model, top_level=False)
+
+            # note: checks all models in a union, but only the first model is significant in RDFProxy
+            elif _is_pydantic_model_union_static_type(union := field_info.annotation):
+                for nested_model in filter(
+                    _is_pydantic_model_static_type, get_args(union)
+                ):
+                    self._run_checks(nested_model, *self._sub_model_checks)
+                    self._visit(nested_model, top_level=False)
+
+            else:
+                continue
+
+        return model
