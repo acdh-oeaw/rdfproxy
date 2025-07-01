a635f9877b641f681b5b7686e19fb95cf8f924ce
Lukas Plank <lupl@tuta.io>
2025-07-01 09:53:06 +0200

    test: adapt tests for model_bool in root models

--- tests/unit/tests_checkers/test_model_checker_model_bool.py
+++ tests/unit/tests_checkers/test_model_checker_model_bool.py
@@ -7,7 +7,7 @@ from rdfproxy.utils.checkers.model_checker import check_model
 from rdfproxy.utils.exceptions import ModelBoolException
 
 
-class InvalidRoot1(BaseModel):
+class ModelBoolWarnRoot1(BaseModel):
     model_config = ConfigDict(model_bool="x")
 
     x: int = 1
@@ -17,14 +17,14 @@ class DummyAggregate(BaseModel):
     pass
 
 
-class InvalidRoot2(BaseModel):
+class ModelBoolWarnRoot2(BaseModel):
     model_config = ConfigDict(model_bool="x", group_by="x")
 
     x: int = 1
     y: list[DummyAggregate]
 
 
-class InvalidRoot3(BaseModel):
+class ModelBoolWarnRoot3(BaseModel):
     model_config = ConfigDict(group_by="x", model_bool="x")
 
     x: int = 1
@@ -122,10 +122,10 @@ class InvalidTypeSub4(BaseModel):
     model_config = ConfigDict(model_bool=object())
 
 
-invalid_root_models = [
-    InvalidRoot1,
-    InvalidRoot2,
-    InvalidRoot3,
+model_bool_warning_root_models = [
+    ModelBoolWarnRoot1,
+    ModelBoolWarnRoot2,
+    ModelBoolWarnRoot3,
 ]
 
 invalid_sub_models = [
@@ -155,9 +155,14 @@ valid_sub_models = [
 ]
 
 
-@pytest.mark.parametrize("invalid_model", invalid_root_models)
+@pytest.mark.parametrize("invalid_model", model_bool_warning_root_models)
 def test_check_invalid_model_bool_root_models(invalid_model):
-    with pytest.raises(ModelBoolException):
+    message = (
+        "Setting model_bool in root models is semantically void. "
+        "The RDFProxy mapping facility considers the model_bool hook "
+        "only for aggregated submodels and model union fields."
+    )
+    with pytest.warns(UserWarning, match=message):
         check_model(invalid_model)
 
 
