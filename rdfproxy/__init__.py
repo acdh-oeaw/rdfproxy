7155018e42b1024df0659a7d2a238a276a56a7a7
Lukas Plank <lupl@tuta.io>
2026-07-06 07:37:00 +0200

    chore: remove ModelBindingsMapper

--- rdfproxy/__init__.py
+++ rdfproxy/__init__.py
@@ -1,4 +1,3 @@
 from rdfproxy.adapter import SPARQLModelAdapter  # noqa: F401
-from rdfproxy.mapper import ModelBindingsMapper  # noqa: F401
 from rdfproxy.utils._types import ConfigDict, SPARQLBinding  # noqa: F401
 from rdfproxy.utils.models import Page, QueryParameters  # noqa: F401
